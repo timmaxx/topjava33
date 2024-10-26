7cc8d9ad785e8562f5467dd78d9706d4c363d080
timmaxx <m.n.timchenko@gmail.com>
2024-10-26 14:46:17 +0300

    5.2 call any MealRestController method from SpringMain

--- src/main/java/ru/javawebinar/topjava/SpringMain.java
+++ src/main/java/ru/javawebinar/topjava/SpringMain.java
@@ -2,10 +2,13 @@ package ru.javawebinar.topjava;
 
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.model.Role;
 import ru.javawebinar.topjava.model.User;
+import ru.javawebinar.topjava.web.meal.MealRestController;
 import ru.javawebinar.topjava.web.user.AdminRestController;
 
+import java.time.LocalDateTime;
 import java.util.Arrays;
 
 public class SpringMain {
@@ -13,8 +16,24 @@ public class SpringMain {
         // java 7 automatic resource management (ARM)
         try (ConfigurableApplicationContext appCtx = new ClassPathXmlApplicationContext("spring/spring-app.xml")) {
             System.out.println("Bean definition names: " + Arrays.toString(appCtx.getBeanDefinitionNames()));
+
             AdminRestController adminUserController = appCtx.getBean(AdminRestController.class);
             adminUserController.create(new User(null, "userName", "email@mail.ru", "password", Role.ADMIN));
+
+            MealRestController mealRestController = appCtx.getBean(MealRestController.class);
+            System.out.println(mealRestController.get(2));
+
+            mealRestController.getAll().forEach(System.out::println);
+
+            mealRestController.create(new Meal(null, 1, LocalDateTime.now(), "Abc", 111));
+            mealRestController.getAll().forEach(System.out::println);
+
+            mealRestController.update(new Meal(4, 1, LocalDateTime.now(), "Qwerty", 111), 4);
+            mealRestController.getAll().forEach(System.out::println);
+
+            mealRestController.delete(5);
+            mealRestController.getAll().forEach(System.out::println);
+
         }
     }
 }
